1ac3adf8634b990d364597bc5821892fd6eec601
Esteban Quijia <eaquijia@puce.edu.ec>
2025-08-17 12:32:24 -0500

    Configura el enrutador principal en App.jsx

--- movilidad-front/src/App.jsx
+++ movilidad-front/src/App.jsx
@@ -1,7 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Usuarios from './pages/Usuarios';
+import Reportes from './pages/Reportes';
+import Acompanamientos from './pages/Acompanamientos';
+import Conductores from './pages/Conductores';
+import Viajes from './pages/Viajes';
 
 function App() {
   return (
@@ -10,6 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/reportes" element={<Reportes />} />
+        <Route path="/acompanamientos" element={<Acompanamientos />} />
+        <Route path="/conductores" element={<Conductores />} />
+        <Route path="/viajes" element={<Viajes />} />
       </Routes>
     </>
   );
