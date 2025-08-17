fb4e13604315c771a8fb8a1fcdad877e087fe1e3
Esteban Quijia <eaquijia@puce.edu.ec>
2025-08-17 12:01:28 -0500

    Creacion de paginas con estructura basica para todas

---
+++ movilidad-front/src/pages/Usuarios.jsx
@@ -0,0 +1,15 @@
+function Usuarios() {
+  return (
+    <div style={{
+      textAlign: 'center',
+      marginTop: '3rem',
+      fontSize: '1.8rem',
+      color: '#004080'
+    }}>
+      <h1>Página de <strong>Usuarios</strong></h1>
+      <p>Aquí se gestionarán los usuarios de la plataforma.</p>
+    </div>
+  );
+}
+
+export default Usuarios;
---
+++ movilidad-front/src/pages/Viajes.jsx
@@ -0,0 +1,15 @@
+function Viajes() {
+  return (
+    <div style={{
+      textAlign: 'center',
+      marginTop: '3rem',
+      fontSize: '1.8rem',
+      color: '#004080'
+    }}>
+      <h1>Página de <strong>Viajes</strong></h1>
+      <p>Aquí se visualizarán los Viajes.</p>
+    </div>
+  );
+}
+
+export default Viajes;
