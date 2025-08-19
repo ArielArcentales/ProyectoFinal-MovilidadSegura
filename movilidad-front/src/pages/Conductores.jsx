ea99f116907fb0ef66640a741a2d5d4e779fede6
Abrahan04 <jamasabanda@puce.edu.ec>
2025-08-18 19:44:17 -0500

    Formulario de conductores

--- movilidad-front/src/pages/Conductores.jsx
+++ movilidad-front/src/pages/Conductores.jsx
@@ -1,13 +1,18 @@
+import React from 'react';
+import ConductorForm from '../components/ConductorForm'; // ¡Importamos nuestro nuevo formulario!
+
 function Conductores() {
   return (
-    <div style={{
-      textAlign: 'center',
-      marginTop: '3rem',
-      fontSize: '1.8rem',
-      color: '#004080'
-    }}>
-      <h1>Página de <strong>Conductores</strong></h1>
-      <p>Aquí se visualizarán los reportes del sistema.</p>
+    <div>
+      {/* Título principal de la página */}
+      <div style={{ textAlign: 'center', marginTop: '3rem', fontSize: '1.8rem', color: '#004080' }}>
+        <h1>Gestión de <strong>Conductores</strong></h1>
+      </div>
+
+      {/* Aquí mostramos el componente del formulario de creación */}
+      <ConductorForm />
+
+      {/* En el futuro, aquí irá la lista de conductores registrados */}
     </div>
   );
 }
